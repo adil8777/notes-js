cf172867d0dfc5f76550647e8191e8f624177aef
adil8777 <adil538466@gmail.com>
2025-01-28 10:06:09 +0530

    Add files via upload

---
+++ Questions.js
@@ -0,0 +1,29 @@
+// let a= 3
+// let b= 1
+// if (b<a){
+// console.log(b, "smallest") ;
+// }
+// else{ 
+//     console.log(a);
+// }
+
+
+// let num = Math.floor(Math.random() * 51)
+
+// if (num > 10 && num <= 50) {
+//     console.log(`${num} is lies between (10,50] `);
+// } else {
+//     console.log(`${num} do not lies`);
+// }
+
+
+// let char = 'u'
+
+// if(char === 'a' || char === 'e' || char === "i" || char === "o" || char === "u" || char === 'A' || char === 'E' || char === "I" || char === "O" || char === "U"){
+//     console.log(`${char} is a vowel`);
+// }else{
+//     console.log(`${char} is consonant`);
+// }
+
+
+
