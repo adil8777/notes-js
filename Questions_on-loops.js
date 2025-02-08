1cee357ae78bdb89f4a0820f61b575613f9a0436
adil8777 <adil538466@gmail.com>
2025-02-08 10:28:49 +0530

    Add files via upload

---
+++ Questions_on-loops.js
@@ -0,0 +1,102 @@
+// Question 1
+
+// for(let i = 1; i<= 100 ; ++i){
+//     console.log(i);
+// }
+
+// 2 x 1 = 2
+// 2 x 2 = 4
+// 2 x 3 = 6 fds fds
+
+
+
+// .
+// .
+// .
+// 2 x 100 = 200
+
+const tableNum = 2
+for (let i=1; i<=100 ; ++i){
+   console.log(`${tableNum} x ${i} = ${i * tableNum}`);
+}
+
+// Question 1
+
+for (let i=1; i<=10; ++i){
+    console.log(`${i}`);
+ }
+
+ 
+// for(let i = 1; i<= 100 ; ++i){
+//     console.log(i);
+// }
+
+// 2 x 1 = 2
+// 2 x 2 = 4
+// 2 x 3 = 6 fds fds
+
+
+
+// .
+// .
+// .
+// 2 x 100 = 200
+
+// const tableNum = 2
+// for (let i=1; i<=100 ; ++i){
+//    console.log(`${tableNum} x ${i} = ${i * tableNum}`);
+// }
+
+// Question 1
+
+// for (let i=1; i<=10; ++i){
+//    console.log(`${i}`);
+// }
+
+// Question 2
+
+// const arr3 = [`sunday`, `monday`, `tuesday`, `wednesday`, `thursday`, `friday`]
+
+// // console.log(arr3[1]);
+
+// for (let i = 0; i < arr3.length; i++) {
+//    console.log(arr3[i]);
+// }
+
+//Question 3
+
+// for(i=1; i<=20; i++){
+//    if(i % 2 === 0){
+//       console.log(i);
+//    }
+// }
+
+// for(i=1; i<=30; i++){
+//    if(i % 3 ===0){
+//       console.log(i);
+//    }
+// }
+
+// Question 4
+
+// let sum = 1 
+
+// sum = sum + 1
+
+// console.log(sum);
+
+// 
+
+// Question 5
+
+// const table = 5
+// for (let i = 1; i <= 10; i++) {
+//    console.log(`${table} * ${i} = ${i * table}`);
+// }
+
+//Question 7
+
+// let arry = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// for (i = arry.length - 1; i >= 0; i--) {
+//    console.log(arry[i]);
+// }
